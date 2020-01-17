dccf31d75a5852de7ab46b65428c006ce5d17080
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-17 05:34:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  data.each do |key, value|
+    value.each do |attribute, names|
+      names.each do |name|
+        result[name] ||= {}
+        result[name][key] ||= []
+        result[name][key] << attribute.to_s
+      end
+    end
+  end
+  result
 end
