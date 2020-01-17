5d5f0c888cb4b21c02bc27d2c0a070807a3e7626
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-17 05:33:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,17 +6,8 @@ def nyc_pigeon_organizer(data)
         result[name] ||= {}
         result[name][key] ||= []
         result[name][key] << attribute.to_s
-        
-  
-    result = {}
-  data.each do |key,value|
-  	value.each do |attribute,names|
-  		names.each do |pigeon|
-  			result[pigeon] ||= {}
-  			result[pigeon][key] ||= []
-  			result[pigeon][key] << attribute.to_s
-  		end
-  	end
+      end
+    end
   end
   result
 end
