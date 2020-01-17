d74fa33c327ac10617e54dae4f5fbe3375999a0c
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-17 05:28:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,8 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
   result = {}
-  data.each do |key,value|   #key is :color, value is {:purple => [...]}
-  	value.each do |attribute,names| #attribute is :purple, names is [...]
-  		names.each do |pigeon| #pigeon is "Theo"
+  data.each do |key,value|
+  	value.each do |attribute,names|
+  		names.each do |pigeon|
   			result[pigeon] ||= {} #{"Theo": }
   			result[pigeon][key] ||= [] #{"Theo" : [...]}
   			result[pigeon][key] << attribute.to_s
